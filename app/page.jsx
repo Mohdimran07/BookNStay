0dd9daa36f26cac050be30f1c15bd8f49f0655f6
Mohdimran07 <mohdimran97057@gmail.com>
2025-06-02 16:03:40 +0530

    Homepage components

---
+++ app/page.jsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import InfoBoxes from "@/components/InfoBoxes";
+import React from "react";
+
+const Home = () => {
+  return (
+    <>
+      <Hero />
+      <InfoBoxes />
+    </>
+  );
+};
+
+export default Home;
